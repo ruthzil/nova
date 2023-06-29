8ac7e812a32a6940fb2ddbcbc3962375c04d7100
root <caio@citrangulo.com.br>
2023-06-29 19:42:03 -0300

    Updte

--- src/main/java/br/com/ruthzil/NovaeApplication.java
+++ src/main/java/br/com/ruthzil/NovaeApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class NovawebApplication {
+public class NovaeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NovawebApplication.class, args);
+		SpringApplication.run(NovaeApplication.class, args);
 	}
 
 }
